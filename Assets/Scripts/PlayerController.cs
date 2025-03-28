0d6b47f421197d131c6c744f9832de4d53e46abe
Yudong Li <a1923172548@gmail.com>
2025-03-28 14:43:04 -0700

    Camera and follow focal point direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerBD;
+    public float speed = 5;
+    public GameObject focalPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBD = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        playerBD.AddForce(focalPoint.transform.forward * verticalInput * speed);
     }
 }
