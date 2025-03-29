a66d8db36383773087dbd97bc4ef728540ae64fc
Yudong Li <a1923172548@gmail.com>
2025-03-28 17:38:19 -0700

    powerup + indicator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public float speed = 5;
     public GameObject focalPoint;
     public bool hasPowerup = false;
+    private float powerupStrength = 15.0f;
+    private float powerupDuration = 7;
+    public GameObject powerUpIndicator;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.436f, 0);
         playerBD.AddForce(focalPoint.transform.forward * verticalInput * speed);
     }
 
@@ -30,14 +36,26 @@ public class PlayerController : MonoBehaviour
             
             Destroy(other.gameObject);
             hasPowerup = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutine());
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
-            Debug.Log("Collide with enemy");
+            Rigidbody enemyBD = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayDirection = collision.gameObject.transform.position - transform.position;
+            enemyBD.AddForce(awayDirection * powerupStrength, ForceMode.Impulse);
+            
         }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerUpIndicator.gameObject.SetActive(false);
     }
 }
